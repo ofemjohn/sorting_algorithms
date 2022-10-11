5565d40f78c2802d851237d47ed8be7d0357f396
ofemjohn <ofemjohn@gmail.com>
2022-10-11 20:27:04 +0100

    sorting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,27 @@
 #include "sort.h"
-
 /**
- * bubble_sort - Calls function
- * @array: Array to be sorted
+ * bubble_sort - Sort an array with buble sort algorithm
+ * @array: Array that going to be sorted
  * @size: Size of array
- * Description: Function that sorts an array using the bubble sort method
- * Return: 0
+ * Return: Nothing, just sort and print when swap
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
+	int tmp;
+	size_t i, j;
 
-	if (!array)
+	if (array == NULL || size == 0)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}
